23fbe284a7ec15960ae23cfcc70d61f6976fe5c2
L.Y. Sim <onlylys@protonmail.com>
2019-10-14 14:32:43 +0800

    Improve formatting of tokenizer code for clarity.

--- src/interpreter/tokenizer/tokenize.ts
+++ src/interpreter/tokenizer/tokenize.ts
@@ -6,57 +6,74 @@ import { TokenStream } from './token-stream';
  * `undefined` is returned.
  */
 export function tokenize(input: string): TokenStream | undefined {
+
     const tokens: Token[] = [];
+
     for (let i = 0; i < input.length; ++i) {
+
         // Skip whitespace.
         if (input[i].trim() === '') {
             continue;
         }
+
         if (isDigit(input[i])) {
             let magnitude = toDigit(input[i]) as number;
             // Consume until the end of the number.
-            while (i + 1 < input.length && isDigit(input[i + 1])) {
-                magnitude = (magnitude * 10) + ++i;
+            while ((i + 1 < input.length) && isDigit(input[i + 1])) {
+                const nextDigit = ++i;
+                magnitude = (magnitude * 10) + nextDigit;
             }
             tokens.push({ kind: 'number', magnitude });
         } 
-        else if (input[i] === '-' && i + 1 < input.length && isDigit(input[i + 1])) {
-            // Increment `i` to skip the `-` sign.
+
+        else if ((input[i] === '-') && (i + 1 < input.length) && isDigit(input[i + 1])) {
+            // Note: we `++i` here to skip the `-` sign.
             let magnitude = toDigit(input[++i]) as number;
-            // Consume the number following the `-` sign.
-            while (i + 1 < input.length && isDigit(input[i + 1])) {
-                magnitude = (magnitude * 10) + ++i;
+            // Consume until the end of the number following the `-` sign.
+            while ((i + 1 < input.length) && isDigit(input[i + 1])) {
+                const nextDigit = ++i;
+                magnitude = (magnitude * 10) + nextDigit;
             }
             tokens.push({ kind: 'negativeNumber', magnitude });
         } 
-        else if (input[i] === ',' || input[i] === ':') {
+
+        else if ((input[i] === ',') || (input[i] === ':')) {
             tokens.push({ kind: 'commaOrColon' });
         } 
+
         else if (input[i] === '.') {
             // Lookahead by 1 character to decide whether the `.` or `..` operator is being used. 
-            if (i + 1 < input.length && input[i + 1] === '.') {
+            if ((i + 1 < input.length) && input[i + 1] === '.') {
                 tokens.push({ kind: 'doublePeriod' });
+                // Consume the second `.`.
                 ++i;
             } else {
                 tokens.push({ kind: 'period' });
             }
         } 
+
         else if (input[i] === 'H') {
             tokens.push({ kind: 'H' });
         } 
+
         else if (input[i] === 'L') {
             tokens.push({ kind: 'L' });
         } 
+        
         else if (input[i] === 'h') {
             tokens.push({ kind: 'h' });
         } 
+
         else if (input[i] === 'l') {
             tokens.push({ kind: 'l' });
         } 
+        
         else {
             return undefined;
         }
+
     }
+
     return new TokenStream(tokens);
 }
 
